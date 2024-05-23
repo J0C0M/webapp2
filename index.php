cdcf7364c493ce2b3e80ccc3ea0431fc4fe17bb2
J0C0M <117586494+J0C0M@users.noreply.github.com>
2024-05-23 10:33:57 +0200

    commentaar index

--- index.php
+++ index.php
@@ -1,12 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="css/webapp2.css">
+    <link rel="stylesheet" href="css/index.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
+    <?php
+    //header include
+    ?>
+
+    <div>
+        <!--slide show van plaatsen-->
+    </div>
     
+    <div>
+        <!--over tab-->
+    </div>
+
+    <?php 
+    //booking
+    ?>
+
+    <?php
+    //include footer
+    ?>
 </body>
 </html>
