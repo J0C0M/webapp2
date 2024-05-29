99d787034fa685785b28e271bec30782b2a3914a
Nikola258 <nikolic07nikola@gmail.com>
2024-05-29 10:25:50 +0200

    footer toegevoegd css changes

--- vakantie.php
+++ vakantie.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="CSS/index.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vakantie</title>
 </head>
 <body>
-    
+    <footer class = "footer">hello this is a test</footer>
 </body>
 </html>
