91d9bd229713373e4c6bd43345e7be679ced04af
Nikola258 <nikolic07nikola@gmail.com>
2024-05-29 12:08:30 +0200

    worked on the footer

--- vakantie.php
+++ vakantie.php
@@ -1,12 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&family=Poetsen+One&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="CSS/index.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vakantie</title>
 </head>
 <body>
-    <footer class = "footer">hello this is a test</footer>
+    <footer>
+
+        <div class = "hold-footer">
+        <div class = "social-media">social media</div>
+
+        <div class = "hold-images"><img class= "tumblr-logo" src="/images/tumblr-svgrepo-com.png" alt="logo of tumblr">
+        <img class= "pinterest-logo" src="/images/pinterest-svgrepo-com.png" alt="logo of pinterest">
+        <img class= "instagram-logo" src="/images/instagram-svgrepo-com.png" alt="logo of instagram">
+        <img class= "youtube-logo" src="/images/youtube-svgrepo-com.png" alt="logo of youtube">
+        </div>
+
+        <div class = "contact-button">contact</div>
+        </div>
+
+        <div class = "hold-div-2">
+
+        </div>
+    </footer>
 </body>
 </html>
