e9cccc553032b902c67375b91f5cc7713dad0cc3
J0C0M <117586494+J0C0M@users.noreply.github.com>
2024-05-31 12:12:29 +0200

    header created + link made for fonts

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <link rel="stylesheet" href="css/index.css">
+    <link href="https://fonts.googleapis.com/css2?family=Concert+One&family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&family=Poetsen+One&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Index page</title>
@@ -9,6 +10,7 @@
 <body>
     <?php
     //header include
+    include("include/header.php");
     ?>
 
     <div>
