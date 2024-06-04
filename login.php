538fd75faddc373c847e21fab14684f55fc010cc
Nikola258 <nikolic07nikola@gmail.com>
2024-06-04 11:27:30 +0200

    test file added

--- login.php
+++ login.php
@@ -1,11 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
+    <link rel="stylesheet" href="CSS/login.css">
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
 </head>
+
 <body>
-    
+        <div class="test">
+        <div>
+            
+        </div>
+    </div>
+    <video class="video" autoplay loop muted plays-inline>
+        <source src="images/login.mp4" type="video/mp4">
+    </video>
 </body>
+
 </html>
--- vakantie.php
+++ vakantie.php
@@ -10,14 +10,20 @@
     <title>Vakantie</title>
 </head>
 
-    <body class="background-image">
+    <body class = "background-image">
+        <video class="video" autoplay loop muted plays-inline>
+            <source src="images/mixkit-bright-orange-sunset-on-beach-2168-full-hd.mp4" type="video/mp4">
+        </video>
+
         <?php
             //header include
             include("include/header.php");
         ?>
+
         <div class = "hold-drop-down-1">
             <div class = drop-down-1></div>
         </div>
+
         <?php
             //header include
             include("include/footer.php");
