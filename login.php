d2428f4b23292c37581841ce2c35dd9ccf2fe600
P9ndabear <1208846@student.roc-nijmegen.nl>
2024-06-06 10:21:35 +0200

    Login page html & CSS

--- login.php
+++ login.php
@@ -2,22 +2,34 @@
 <html lang="en">
 
 <head>
-    <link rel="stylesheet" href="CSS/login.css">
+    <link rel="stylesheet" href="CSS/index.css">
+    <link href="https://fonts.googleapis.com/css2?family=Concert+One&family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&family=Poetsen+One&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
 </head>
+<?php include("include/header.php"); ?>
 
 <body>
-        <div class="test">
-        <div>
-            
+    <div class="form-container">
+        <div class="form-box">
+            <p class="login-introduction">
+                Hallo,
+            </p>
+            <p class="login-introduction">
+                Welkom!
+            </p>
+            <form>
+                <input type="email" class="email-box" placeholder="Email">
+                <input type="password" class="password-box" placeholder="Wachtwoord">
+            </form>
         </div>
     </div>
     <video class="video" autoplay loop muted plays-inline>
         <source src="images/login.mp4" type="video/mp4">
     </video>
 </body>
+<?php include("include/header.php"); ?>
 
 </html>
