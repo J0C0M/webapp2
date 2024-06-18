426e8c0b1b755c2fa4eab8927ea9836aedfd9aa7
Nikola258 <nikolic07nikola@gmail.com>
2024-06-18 10:36:13 +0200

    test.php en style css edits

--- test.php
+++ test.php
@@ -1,37 +1,92 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&family=Poetsen+One&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="CSS/index.css">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Vakantie</title>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="CSS/style.css">
+    <title>Go Trip</title>
 </head>
 
-    <body class = "background-image">
-        <video class="video" autoplay loop muted plays-inline>
-            <source src="images/mixkit-bright-orange-sunset-on-beach-2168-full-hd.mp4" type="video/mp4">
-        </video>
+<body>
+
+    <body>
 
         <?php
             //header include
             include("include/header.php");
         ?>
-
-        <div class = "hold-drop-down-1">
-            <div class = drop-down-1></div>
+        
+        <!--===============Banner================-->
+        <section class="banner">
+    <video autoplay muted loop class="banner-video">
+        <source src="images/mixkit-bright-orange-sunset-on-beach-2168-full-hd.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+    </video>
+    <div class="banner-text-item">
+        <div class="banner-heading">
+            <h1>Find your Next tour!</h1>
         </div>
-
-        <div class = "hold-drop-down-2">
-            <div class = drop-down-2></div>
+        <form class="form">
+            <input type="text" list="mylist" placeholder="Where would you like to go?">
+                <datalist id="mylist">
+                    <option>London</option>
+                    <option>Canada</option>
+                    <option>Monaco</option>
+                    <option>France</option>
+                    <option>Japan</option>
+                    <option>Switzerland</option>
+                    <option>Seoul</option>
+                </datalist>
+                <input type="date" class="date">
+                <a href="#" class="book">book</a>
+            </form>
         </div>
+    </section>
 
-        <?php
-            //header include
-            include("include/footer.php");
-        ?>
+    <!--
+        
 
+
+    -->
+
+        <!--===========Footer=================-->
+        <div class="footer">
+            <div class="links">
+                <h3>Quick Links</h3>
+                <ul>
+                    <li>Offers & Discounts</li>
+                    <li>Get Coupon</li>
+                    <li>Contact Us</li>
+                    <li>About</li>
+                </ul>
+            </div>
+            <div class="links">
+                <h3>New Products</h3>
+                <ul>
+                    <li>Woman Cloth</li>
+                    <li>Fashion Accessories</li>
+                    <li>Man Accessories</li>
+                    <li>Rubber made Toys</li>
+                </ul>
+            </div>
+            <div class="links">
+                <h3>Support</h3>
+                <ul>
+                    <li>Frequently Asked Questions</li>
+                    <li>Report a Payment Issue</li>
+                    <li>Terms & Conditions</li>
+                    <li>Privacy Policy</li>
+                </ul>
+            </div>
+        </div>
+    </body>
+
+</html>
 </body>
+
 </html>
