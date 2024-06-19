c55c3dfd67c39fe0bb30bccd7c555580135dd58a
P9ndabear <1208846@student.roc-nijmegen.nl>
2024-06-19 11:18:45 +0200

    session start toegevoegd en type kleur veranderd in inlog page

--- admin.php
+++ admin.php
@@ -1,12 +1,24 @@
+<?php
+    session_start();
+    if(isset($_SESSION['username'])) {
+
+    } else {
+        header(header: "Location: login.php"    );
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <link rel="stylesheet" href="css/webapp2.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     skibidi
 </body>
+
 </html>
