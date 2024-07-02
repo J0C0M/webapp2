7b4e26752f3fd908c5d97bc01a6bd11b51f31965
P9ndabear <1208846@student.roc-nijmegen.nl>
2024-07-02 10:49:37 +0200

    adding forgot password

--- forgotPassword.php
+++ forgotPassword.php
@@ -6,29 +6,19 @@ session_start();
 ?>
 <?php
 if (isset($_POST['login'])) {
-    if (empty($_POST["email"]) || empty($_POST["password"])) {
+    if (empty($_POST["email"])) {
         die("Er moet iets ingevuld worden.");
     } else {
-        $query = "SELECT * FROM user WHERE email = :email AND password = :password";
+        $query = "SELECT * FROM user WHERE email = :email";
         $stmt = $pdo->prepare($query);
-        $stmt->bindParam(":email", $_POST['email']);
-        $stmt->bindParam(":password", $_POST['password']);
+        $stmt->bindParam(":email", $_POST['email']); 
         $stmt->execute();
         $user = $stmt->fetch();
         if ($user) {
             $_SESSION['email'] = $user['email'];
-            $_SESSION['admin'] = $user['admin'];
-        }
-        if ($user['admin'] == '0') {
-            header("Location: account.php");
-        }
-
-        if ($user['admin'] == '1') {
-            header("Location: admin.php");
         }
     }
 }
-
 ?>
 
 <!DOCTYPE html>
@@ -57,10 +47,7 @@ if (isset($_POST['login'])) {
             </div>
             <form name="login-form" action="login.php" method="post" class="login-form">
                 <input type="email" id="email" name="email" class="login-name-box" placeholder="email">
-                <input type="password" id="password" name="password" class="login-password-box" placeholder="Wachtwoord">
                 <div class="login-button-box">
-                    <a class="login-forgot-password-button" href="forgotPassword.php">Wachtwoord vergeten</a>
-                    <a class="login-signup-button" href="signup.php">Sign up</a>
                     <button value="submit" type="submit" name="login" class="login-submit-button">Log in</button>
                 </div>
             </form>
