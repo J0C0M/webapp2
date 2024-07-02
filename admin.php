06b996e588ef47e3b301d1c1829233713a028233
Nikola258 <nikolic07nikola@gmail.com>
2024-07-02 20:58:49 +0200

    admin page is done (header moet nog gefixt worden)

--- admin.php
+++ admin.php
@@ -12,7 +12,7 @@ if (isset($_SESSION['admin']) && $_SESSION['admin'] === 1) {
 <html lang="en">
 
 <head>
-    <link rel="stylesheet" href="CSS/footer-header.css">
+    <link rel="stylesheet" href="CSS/admin.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
