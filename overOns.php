6d89da9493584f7c1419869585203d08ccccb971
J0C0M <117586494+J0C0M@users.noreply.github.com>
2024-07-03 10:02:14 +0200

    Over Ons pagina

--- overOns.php
+++ overOns.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/OverOns.css">
     <link href="https://fonts.googleapis.com/css2?family=Concert+One&family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&family=Poetsen+One&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -15,8 +15,20 @@
 
 <div class="over-ons-home">
         <!--search bar-->
-        <h1>Boek snel een crazy vakantie bij Epic Vacations</h1>
+        <h1>Boek snel een crazy vakantie bij Epic Vacations!</h1>
     </div>
+
+    <div class="container">
+        <div class="content">
+            <h2>Over Ons</h2>
+            <p>Epic Vacations is opgericht door Jochem, Nikola en David. Wij zijn een unieke vakantie boeken website waar je reizen naar speciale locaties zoals Gotham City, het Fortnite Island en nog veel meer kunt boeken.</p>
+            <p>Ons doel is om onze klanten onvergetelijke ervaringen te bieden door hen naar de meest fascinerende en avontuurlijke bestemmingen te brengen.</p>
+            <p>Met ons gepassioneerde team en uitgebreide kennis van reizen, zorgen we ervoor dat elke vakantie die je bij ons boekt een episch avontuur wordt!</p>
+            <a href="contact.php" class="button">Neem Contact Op</a>
+        </div>
+    </div>
+
+
 
     <?php
     //header include
